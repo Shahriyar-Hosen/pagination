057e1112b6d393551af197f7a8708afa2d012158
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-03 08:34:19 +0600

    page_number

--- index.js
+++ index.js
@@ -12,16 +12,23 @@ const array = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
+const pageSize = 5;
+let pageNumber;
+
 /* 
-page_number = How many pages will there be?
+page_number (4) = How many pages will there be?
 
 page_size = How much content to show on a page 
 */
 
-console.log(pagination(array, 5, 4));
+console.log(pagination(array, pageSize, pageNumber));
 
-// page_number_ Finding = Finding the number of pages according to the length of the array
+// page_number_Finding = Finding the number of pages according to the length of the array
 
-const page = array.length / 5; // 5 === page_size;
+const page = array.length / pageSize; // 5 === page_size;
 
-[...Array(page).keys()].map((num) => <div key={num}>{num + 1}</div>);
+[...Array(page).keys()].map((num) => (
+  <div key={num} onclick={() => (pageNumber = num + 1)}>
+    {num + 1}
+  </div>
+));
