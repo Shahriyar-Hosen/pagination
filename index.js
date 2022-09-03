3460185d179528e04604214986290b6bcec8cf0e
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-03 08:29:23 +0600

    page_number_ Finding

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const pagination = (array, page_size, page_number) => {
   return data;
 };
 
-const abc = [
+const array = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
@@ -18,4 +18,10 @@ page_number = How many pages will there be?
 page_size = How much content to show on a page 
 */
 
-console.log(pagination(abc, 5, 4));
+console.log(pagination(array, 5, 4));
+
+// page_number_ Finding = Finding the number of pages according to the length of the array
+
+const page = array.length / 5; // 5 === page_size;
+
+[...Array(page).keys()].map((num) => <div key={num}>{num + 1}</div>);
