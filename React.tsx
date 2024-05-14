7e3f16e1178daa374fc65c11e237d7b98988bd28
Shahriyar Hosen <96829173+Shahriyar-Hosen@users.noreply.github.com>
2024-05-14 09:48:22 +0600

    Update and rename React.js to React.tsx

--- React.tsx
+++ React.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store"; // Assuming RootState contains the type definitions for your Redux state
 import { paginateNumber } from "../../features/paginate/paginateSlice";
 
-// pagination with tailwind css
-
-const Pagination = () => {
+const Pagination: React.FC = () => { // Define component as React functional component
   const dispatch = useDispatch();
 
-  const { pageNumber } = useSelector((state) => state.paginate);
+  const { pageNumber }: { pageNumber: number } = useSelector((state: RootState) => state.paginate); // Specify type for pageNumber
 
-  const page = useSelector((state) =>
+  const page: number = useSelector((state: RootState) =>
     Math.ceil(state.videos.videos.length / 5)
-  );
+  ); // Specify type for page
 
-  const handlePageSelect = (pageNumber) => {
+  const handlePageSelect = (pageNumber: number) => {
     dispatch(paginateNumber(pageNumber));
   };
 
   return (
     <section className="pt-12">
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end">
-        {[...Array(page).keys()].map((num) => (
+        {[...Array(page).keys()].map((num: number) => ( // Specify type for num
           <div
             key={num}
             className={`${
